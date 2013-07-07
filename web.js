34d9a1c879ec65a3be4b187ff25ef804992cb3f9
Neil Kutchera <neil.kutchera@gmail.com>
2013-07-07 02:54:42 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fileContents = "";
+
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  // console.log(data);
+  fileContents = data;
+});
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  // response.send('Hello World 2!');
+  response.sent(fileContents);
 });
 
 var port = process.env.PORT || 5000;
